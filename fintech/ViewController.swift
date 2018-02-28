981fbe166fbb696dc527548bd86f5145bdf8f98b
Maxim Pestryakov <maximpestryakov@gmail.com>
2018-02-28 23:16:15 +0300

    Initial Commit

---
+++ fintech/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  fintech
+//
+//  Created by m.pestryakov on 28/02/2018.
+//  Copyright © 2018 MaximPestryakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
